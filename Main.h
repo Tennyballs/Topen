3c1ae9e928b42b775b6b0b500534858e3665935c
Cuppy130 <player130games@gmail.com>
2025-08-23 17:26:01 -0700

    makes sure that the terraira world is made by the terraria game

--- Main.h
+++ Main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define println(v) printf("%s\n", v)
+#define print(v) printf("%s", v)
+
 int main(int argc, char **argv);
+
+#ifndef WORLD_VERSION
+#define WORLD_VERSION 217
+#endif
 
 #endif
